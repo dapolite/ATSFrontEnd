d7e8c7f0f541934c000ca16ec7ac9b810de81f49
dapolite <ankurbanerjeeofficial@gmail.com>
2020-04-15 19:35:09 +0530

    Halfway Axios Post working yet to see if adds to other details

--- login-form-react/src/component/LoginRegister/RegisterFormRecruiter.js
+++ login-form-react/src/component/LoginRegister/RegisterFormRecruiter.js
@@ -5,29 +5,67 @@ import SUDH_logo_1 from './SUDH_logo_1.png';
 import history from '../history';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import axios from 'axios';
 
 export default class RegisterFormRecruiter extends Component{
-    state = {
-        fname: '',
-        lname: '',
-        uname: '',
-        phonenumber: '',
-        dateofbirth: '',
-        email: '',
-        password: '',
-        cpassword: ''
+
+  constructor(props){
+    super(props);
+    this.state = {
+      firstname: '',
+      lastname: '',
+      user_name: '',
+      phoneno: '',
+      dateofbirth: '',
+      email: '',
+      password: '',
+      cpassword: ''
+    };
+    this.handleSubmit=this.handleSubmit.bind(this);
+    this.changeHandler=this.changeHandler.bind(this);
+  }
+
+  changeHandler = event => {
+      this.setState({ [event.target.name]: event.target.value });
+    };
+
+  handleSubmit =event => {
+        
+      const recruiter = {
+        firstname: this.state.firstname,
+        lastname:this.state.lastname,
+        user_name:this.state.user_name,
+        phoneno:this.state.phoneno,
+        dateofbirth:this.state.dateofbirth,
+        email:this.state.email,
+        password:this.state.password
       };
-    
-      handleSubmit =event => {
-        history.push('/RegisterFormRecruiterSecondPage')
-        event.preventDefault();
-      }
-    
-      changeHandler = event => {
-        this.setState({ [event.target.name]: event.target.value });
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
       };
+
+      axios.post("http://localhost:8080/api/Recruiter",recruiter,config)
+      .then(res => {
+        if(res.data!=null){
+          console.log(res.data);
+          console.log(recruiter);
+          console.log(res);
+          alert("Rec Added");
+          alert(recruiter);
+        }
+
+      })
+      
+      event.preventDefault();
+      //history.push('/RegisterFormRecruiterSecondPage')
+    }
+  
     
-      render(){
+    render(){
+        const {firstname,lastname,user_name,phoneno,dateofbirth,email,password}=this.state
         return(
           <div>
             <div>
@@ -39,7 +77,7 @@ export default class RegisterFormRecruiter extends Component{
               <h5 class="h5" align="center">Please complete to create your account</h5><br></br>
               <Row>
                 <Col>
-                  <input type="text" className="form-control" name="fname" placeholder="First Name" value={this.state.fname} onChange={this.changeHandler} required />
+                  <input type="text" className="form-control" name="firstname" placeholder="First Name" value={firstname} onChange={this.changeHandler} required />
                   <div className="invalid-feedback">
                     Please Enter Your First Name
                   </div>
@@ -47,7 +85,7 @@ export default class RegisterFormRecruiter extends Component{
                 </Col>
     
                 <Col>
-                  <input type="text" className="form-control" name="lname" placeholder="Last Name" value={this.state.lname} onChange={this.changeHandler} required />
+                  <input type="text" className="form-control" name="lastname" placeholder="Last Name" value={lastname} onChange={this.changeHandler} required />
                   <div className="invalid-feedback">
                     Please Enter Your Last Name
                   </div>
@@ -57,7 +95,7 @@ export default class RegisterFormRecruiter extends Component{
               <br/>
               <Row>
                 <Col>
-                  <input type="text" className="form-control" name="uname" placeholder="Username" value={this.state.uname} onChange={this.changeHandler} required />
+                  <input type="text" className="form-control" name="user_name" placeholder="Username" value={user_name} onChange={this.changeHandler} required />
                   <div className="invalid-feedback">
                     Please Enter Your User Name
                   </div>
@@ -67,7 +105,7 @@ export default class RegisterFormRecruiter extends Component{
               <br/>
               <Row>
                 <Col>
-                <input type="tel" pattern="[0-9]{5}[0-9]{5}" className="form-control" name="phonenumber" placeholder="Phone Number" value={this.state.phonenumber} onChange={this.changeHandler} required />
+                <input type="tel" pattern="[0-9]{5}[0-9]{5}" className="form-control" name="phoneno" placeholder="Phone Number" value={phoneno} onChange={this.changeHandler} required />
                   <div className="invalid-feedback">
                     Please Enter Your Mobile Number
                   </div>
@@ -75,7 +113,7 @@ export default class RegisterFormRecruiter extends Component{
                 </Col>
     
                 <Col>
-                <input type="date" className="form-control" name="dateofbirth" placeholder="Date Of Birth" value={this.state.dateofbirth} onChange={this.changeHandler} required />
+                <input type="date" className="form-control" name="dateofbirth" placeholder="Date Of Birth" value={dateofbirth} onChange={this.changeHandler} required />
                   <div className="invalid-feedback">
                     Please Enter Your Enter Your Date Of Birth
                   </div>
@@ -85,7 +123,7 @@ export default class RegisterFormRecruiter extends Component{
               <br/>
               <Row>
                 <Col>
-                  <input type="text" pattern="^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$" className="form-control" name="email" placeholder="Email Address" value={this.state.email} onChange={this.changeHandler} required />
+                  <input type="text" pattern="^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$" className="form-control" name="email" placeholder="Email Address" value={email} onChange={this.changeHandler} required />
                   <div className="invalid-feedback">
                     Please Enter Your Email Address
                   </div>
@@ -95,7 +133,7 @@ export default class RegisterFormRecruiter extends Component{
               <br/>
               <Row>
                 <Col>
-                  <input type="password" className="form-control" name="password" placeholder="Password" value={this.state.password} onChange={this.changeHandler} required />
+                  <input type="password" className="form-control" name="password" placeholder="Password" value={password} onChange={this.changeHandler} required />
                   <div className="invalid-feedback">
                     Please Enter Your Password
                   </div>
